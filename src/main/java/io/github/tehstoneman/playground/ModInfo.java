6d153d24bd958735c63daa39846d99d104fc6954
Peter McMurray <stonemac@live.com.au>
2017-11-26 15:58:18 +1100

    Minor change

--- src/main/java/io/github/tehstoneman/playground/ModInfo.java
+++ src/main/java/io/github/tehstoneman/playground/ModInfo.java
@@ -5,7 +5,7 @@ public class ModInfo
 	public static final String	MODID			= "playground";
 	public static final String	NAME			= "Stoney's Playground";
 	public static final String	VERSION			= "mc1.11.2-0.0.0.0";
-	public static final String	DEPENDENCIES	= "required-after:";
+	public static final String	DEPENDENCIES	= "required-after:forge@[13.20.0.2228,);";
 	public static final String	MINECRAFT		= "[1.11.2,)";
 	public static final String	PROXY_LOCATION	= "io.github.tehstoneman.playground.proxies.";
 }
